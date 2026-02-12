77803f86f73b0fe7d3a7e8a1d1e94efdbc75b500
lima1909 <linke.mario@web.de>
2026-02-12 21:04:19 +0100

    add first impl of an TrigramIndex

---
+++ trigramindex.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"strings"
+)
+
+type sbucket struct {
+	s        string
+	occupied bool
+}
+
+type TrigramIndex struct {
+	index   map[uint32]BitSet[uint32]
+	buckets []sbucket
+	len     int
+}
+
+func NewTrigramIndex(s ...string) TrigramIndex {
+	ti := TrigramIndex{
+		index:   make(map[uint32]BitSet[uint32], len(s)),
+		buckets: make([]sbucket, 0, len(s)),
+	}
+
+	for i, el := range s {
+		ti.Put(el, i)
+	}
+
+	return ti
+}
+
+func (ti *TrigramIndex) Get(query string) *BitSet[uint32] {
+	resultBS := NewBitSet[uint32]()
+
+	if len(query) < 3 {
+		// full table scan
+		for i, b := range ti.buckets {
+			if b.occupied && strings.Contains(b.s, query) {
+				resultBS.Set(uint32(i))
+			}
+		}
+		return resultBS
+	}
+
+	// generate trigrams for the query
+	for i := 0; i < len(query)-2; i++ {
+		tri := pack(query[i], query[i+1], query[i+2])
+		bs, ok := ti.index[tri]
+		if !ok {
+			// If any trigram doesn't exist, the whole substring can't exist
+			return NewBitSet[uint32]()
+		}
+		resultBS.Or(&bs)
+
+	}
+
+	// verification (False Positive Check)
+	// Trigrams only prove the characters exist; we must verify the order/presence
+	resultBS.Values(func(i uint32) bool {
+		b := ti.buckets[i]
+		if b.occupied && !strings.Contains(b.s, query) {
+			resultBS.UnSet(i)
+		}
+		return true
+	})
+
+	return resultBS
+}
+
+func (ti *TrigramIndex) Put(s string, li int) {
+	if li == ti.len {
+		ti.buckets = append(ti.buckets, sbucket{s: s, occupied: true})
+	} else if li < ti.len {
+		ti.buckets[li] = sbucket{s: s, occupied: true}
+	} else {
+		newBuckets := make([]sbucket, li+1)
+		copy(newBuckets, ti.buckets)
+		ti.buckets = newBuckets
+		ti.buckets[li] = sbucket{s: s, occupied: true}
+	}
+	ti.len++
+
+	for j := 0; j < len(s)-2; j++ {
+		tri := pack(s[j], s[j+1], s[j+2])
+		bs, found := ti.index[tri]
+		if !found {
+			bs = *NewBitSet[uint32]()
+		}
+		bs.Set(uint32(li))
+		ti.index[tri] = bs
+	}
+}
+
+func (ti *TrigramIndex) Delete(li int) bool {
+	if li >= len(ti.buckets) {
+		return false
+	}
+
+	s := ti.buckets[li].s
+
+	for j := 0; j < len(s)-2; j++ {
+		tri := pack(s[j], s[j+1], s[j+2])
+		if bs, found := ti.index[tri]; found {
+			bs.UnSet(uint32(li))
+			ti.index[tri] = bs
+			bs.Shrink()
+		}
+	}
+	ti.buckets[li] = sbucket{s: ""}
+	ti.len--
+
+	return true
+}
+
+func (ti *TrigramIndex) Len() int { return ti.len }
+
+// pack converts 3 bytes into a single uint32 to save memory and speed up lookups
+//
+//go:inline
+func pack(a, b, c byte) uint32 { return uint32(a)<<16 | uint32(b)<<8 | uint32(c) }
---
+++ trigramindex_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTrigram_Base(t *testing.T) {
+	ti := NewTrigramIndex("apple", "apply", "ban", "banana", "xapp")
+	assert.Equal(t, 5, ti.Len())
+	assert.Equal(t, []uint32{0, 1, 4}, ti.Get("app").ToSlice())
+	assert.Equal(t, []uint32{2, 3}, ti.Get("an").ToSlice())
+	// not found
+	assert.Equal(t, []uint32{}, ti.Get("nix").ToSlice())
+
+	assert.True(t, ti.Delete(2))
+	assert.Equal(t, 4, ti.Len())
+	// search with 2 letters
+	assert.Equal(t, []uint32{3}, ti.Get("an").ToSlice())
+
+	// grow the bucket list
+	ti.Put("xban", 20)
+	assert.Equal(t, 5, ti.Len())
+	assert.Equal(t, []uint32{3, 20}, ti.Get("ban").ToSlice())
+
+	// reuse index 2
+	ti.Put("xappx", 2)
+	assert.Equal(t, 6, ti.Len())
+	assert.Equal(t, []uint32{0, 1, 2, 4}, ti.Get("app").ToSlice())
+
+	// checks the false positive: ABCD and BCDE matching {0, 2}
+	ti = NewTrigramIndex("ABCD", "ZZZ", "BCDE")
+	assert.Equal(t, []uint32{}, ti.Get("ABCDE").ToSlice())
+
+	// empty init
+	ti = NewTrigramIndex()
+	assert.Equal(t, 0, ti.Len())
+	assert.Equal(t, []uint32{}, ti.Get("nix").ToSlice())
+
+	ti.Put("üöß€ä@", 2)
+	assert.Equal(t, 1, ti.Len())
+	assert.Equal(t, []uint32{2}, ti.Get("öß€ä").ToSlice())
+}
